51f80bed62502f58cf3a94903280df240a0e3564
Emmanuel Phiri <137718008+Normious@users.noreply.github.com>
2024-11-19 07:02:27 +0200

    first commit

---
+++ database/migrations/2022_01_27_140735_create_conversations_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+    
+            $table->unsignedBigInteger('sender_id');
+            $table->unsignedBigInteger('receiver_id');
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+            
+            // Allow last_time_message to be nullable
+            $table->timestamp('last_time_message')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            if (Schema::hasColumn('messages', 'conversation_id')) {
+                $table->dropForeign(['conversation_id']);
+                $table->dropColumn('conversation_id');
+            }
+        });
+    }
+    
+}
